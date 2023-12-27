5d55d5f930780ee7eeee83eb79eec4c21b6b0ab8
Dewakayika <kayikadewa@gmail.com>
2023-12-27 20:16:05 +0800

    Initial commit

---
+++ app/src/main/java/com/ca214/uasproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ca214.uasproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
